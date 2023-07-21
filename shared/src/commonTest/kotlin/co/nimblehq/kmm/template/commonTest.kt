6467abe2fe49d4a4bc7fa808a7ff3f5e0c053b7a
Luong Vo <luong@nimblehq.co>
2023-07-21 15:26:28 +0700

    [#1] Initialize the new KMM Template project

---
+++ shared/src/commonTest/kotlin/co/nimblehq/kmm/template/commonTest.kt
@@ -0,0 +1,12 @@
+package co.nimblehq.kmm.template
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CommonGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("Hello"), "Check 'Hello' is mentioned")
+    }
+}
---
+++ shared/src/iosTest/kotlin/co/nimblehq/kmm/template/iosTest.kt
@@ -0,0 +1,12 @@
+package co.nimblehq.kmm.template
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class IosGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("iOS"), "Check iOS is mentioned")
+    }
+}
