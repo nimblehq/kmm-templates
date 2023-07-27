300bdb14072c6a03f627dce67f27aa4d9885fad3
lydiasama <hallelujah.lydia@gmail.com>
2023-07-27 18:35:56 +0700

    [#4] Set up detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,36 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id(Plugins.ANDROID_APPLICATION).version(Versions.GRADLE).apply(false)
     id(Plugins.ANDROID_LIBRARY).version(Versions.GRADLE).apply(false)
+    id(Plugins.DETEKT).version(Versions.DETEKT)
     kotlin(Plugins.ANDROID).version(Versions.KOTLIN).apply(false)
     kotlin(Plugins.MULTIPLATFORM).version(Versions.KOTLIN).apply(false)
+}
+
+detekt {
+    toolVersion = Versions.DETEKT
+    config.setFrom("detekt.yml")
+
+    source.setFrom(
+        "android/src",
+        "shared/src"
+    )
+    parallel = false
+
+    buildUponDefaultConfig = false
+    disableDefaultRuleSets = false
+
+    debug = false
+    ignoreFailures = false
+    ignoredBuildTypes = listOf("release")
+    ignoredFlavors = listOf("production")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    jvmTarget = JavaVersion.VERSION_11.toString()
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+    }
 }
 
 tasks.register("clean", Delete::class) {
