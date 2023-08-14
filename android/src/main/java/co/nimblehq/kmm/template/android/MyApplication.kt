d1c1cc7b1a41535738135d9ab91c629dd7835baf
Luong Vo <luong@nimblehq.co>
2023-08-14 11:39:09 +0700

    [#2] Stop using BuildConfig from Gradle 8

--- android/src/main/java/co/nimblehq/kmm/template/android/MyApplication.kt
+++ android/src/main/java/co/nimblehq/kmm/template/android/MyApplication.kt
@@ -1,18 +1,12 @@
 package co.nimblehq.kmm.template.android
 
 import android.app.Application
-import timber.log.Timber
+import co.nimblehq.kmm.template.android.util.LogUtil
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        setupLogging()
-    }
-
-    private fun setupLogging() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
+        LogUtil.setUpLogging()
     }
 }
